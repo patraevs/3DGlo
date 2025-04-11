4764d0d26aad7e96ae6ffaf47858bb03bc59cfc4
patraevs <nvnesh@yandex.ru>
2025-04-11 12:48:42 +0300

    Параметры документа, окна и работа с ними

--- src/modules/modal.js
+++ src/modules/modal.js
@@ -1,17 +1,30 @@
 const modal = () => {
     const modal = document.querySelector('.popup')
+    const modalWindow = modal.querySelector('.popup-content')
+    const width = document.documentElement.clientWidth
+
     const buttons = document.querySelectorAll('.popup-btn')
     const closeBtn = modal.querySelector('.popup-close')
+
+    let count = 0
 
     buttons.forEach(btn => {
         btn.addEventListener('click', () => {
             modal.style.display = 'block'
+            if (width > 768) animacion()
         })
     })
 
     closeBtn.addEventListener('click', () => {
         modal.style.display = 'none'
+        count = 0
     })
+
+    const animacion = () => {
+        count++
+        modalWindow.style.top = count * 2 + 'px'
+        if (count < 100) setTimeout(animacion, 10)
+    }
 }
 
 export default modal
