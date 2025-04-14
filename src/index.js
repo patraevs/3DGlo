f424da8e1e937b217dd048b110de46569b2a7285
patraevs <nvnesh@yandex.ru>
2025-04-14 23:45:39 +0300

    Слайдер

--- src/index.js
+++ src/index.js
@@ -3,9 +3,11 @@ import menu from './modules/menu'
 import modal from './modules/modal'
 import validation from './modules/validation'
 import tabs from './modules/tabs'
+import slider from './modules/slider'
 
 timer('15 april 2025')
 menu()
 modal()
 validation()
 tabs()
+slider()
