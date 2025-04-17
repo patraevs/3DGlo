400444b4f03bdd61008adf5a30db73390d47f5e6
patraevs <nvnesh@yandex.ru>
2025-04-17 23:57:37 +0300

    start

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import tabs from './modules/tabs'
 import slider from './modules/slider'
 import { sliderCarousel } from './modules/sliderCarousel'
 import calc from './modules/calc'
+import sendForm from './modules/sendForm'
 
 timer('17 april 2025')
 menu()
@@ -15,3 +16,13 @@ tabs()
 slider()
 sliderCarousel()
 calc(100)
+sendForm({
+    formId: 'form1',
+    someElem: [
+        {
+            type: 'block',
+            id: 'total'
+        }
+    ]
+})
+
