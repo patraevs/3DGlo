a39086348900417ece60a55d53caa81c781d7cb0
patraevs <nvnesh@yandex.ru>
2025-04-29 15:26:06 +0300

    Async/Await, Try/Catch.

--- src/modules/validation.js
+++ src/modules/validation.js
@@ -1,70 +1,29 @@
 const validation = () => {
+  const validateNumbers = (e) => {
+    e.target.value = e.target.value.replace(/\D+/g, "");
+  };
 
-    const totalArea = document.querySelector('.calc-item.calc-square')
-    totalArea.addEventListener('input', (e) => {
-        e.target.value = e.target.value.replace(/\D+/, '');
-    })
+  const validateEmailChars = (e) => {
+    e.target.value = e.target.value.replace(/[^a-zA-Z0-9@\-_.!~*']/g, "");
+  };
 
-    const numberOfRooms = document.querySelector('.calc-item.calc-count')
-    numberOfRooms.addEventListener('input', (e) => {
-        e.target.value = e.target.value.replace(/\D+/, '');
-    })
+  const numberInputs = [
+    ".calc-item.calc-square",
+    ".calc-item.calc-count",
+    ".calc-item.calc-day",
+  ];
 
-    const dueDate = document.querySelector('.calc-item.calc-day')
-    dueDate.addEventListener('input', (e) => {
-        e.target.value = e.target.value.replace(/\D+/, '');
-    })
+  numberInputs.forEach((selector) => {
+    const input = document.querySelector(selector);
+    input.addEventListener("input", validateNumbers);
+  });
 
-    // const formNameOne = document.getElementById('form1-name')
-    // formNameOne.addEventListener('input', (e) => {
-    //     e.target.value = e.target.value.replace(/[^а-яА-Я\- ]/g, '');
-    // })
+  const emailInputs = ["form1-email", "form2-email", "form3-email"];
 
-    // const formNameTwo = document.getElementById('form2-name')
-    // formNameTwo.addEventListener('input', (e) => {
-    //     e.target.value = e.target.value.replace(/[^а-яА-Я\- ]/g, '');
-    // })
+  emailInputs.forEach((id) => {
+    const input = document.getElementById(id);
+    input.addEventListener("input", validateEmailChars);
+  });
+};
 
-    // const formnameThree = document.getElementById('form3-name')
-    // formnameThree.addEventListener('input', (e) => {
-    //     e.target.value = e.target.value.replace(/[^а-яА-Я\- ]/g, '');
-    // })
-
-    // const inputMessage = document.getElementById('form2-message')
-    // inputMessage.addEventListener('input', (e) => {
-    //     e.target.value = e.target.value.replace(/[^а-яА-Я\- ]/g, '');
-    // })
-
-    const inputEmailOne = document.getElementById('form1-email')
-    inputEmailOne.addEventListener('input', (e) => {
-        e.target.value = e.target.value.replace(/[^a-zA-Z0-9@\-_.!~*']/g, '');
-    })
-
-    const inputEmailTwo = document.getElementById('form2-email')
-    inputEmailTwo.addEventListener('input', (e) => {
-        e.target.value = e.target.value.replace(/[^a-zA-Z0-9@\-_.!~*']/g, '');
-    })
-
-    const inputEmailThree = document.getElementById('form3-email')
-    inputEmailThree.addEventListener('input', (e) => {
-        e.target.value = e.target.value.replace(/[^a-zA-Z0-9@\-_.!~*']/g, '');
-    })
-
-    // const inputPhoneOne = document.getElementById('form1-phone')
-    // inputPhoneOne.addEventListener('input', (e) => {
-    //     e.target.value = e.target.value.replace(/[^0-9()\-]/g, '');
-    // })
-
-    // const inputPhoneTwo = document.getElementById('form2-phone')
-    // inputPhoneTwo.addEventListener('input', (e) => {
-    //     e.target.value = e.target.value.replace(/[^0-9()\-]/g, '');
-    // })
-
-    // const inputPhoneThree = document.getElementById('form3-phone')
-    // inputPhoneThree.addEventListener('input', (e) => {
-    //     e.target.value = e.target.value.replace(/[^0-9()\-]/g, '');
-    // })
-
-}
-
-export default validation
+export default validation;
